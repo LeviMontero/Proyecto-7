a41b42c25b54925cd83845cb7d7c7dc8d9aa3b8f
LeviMontero <levidelosmonteros@gmail.com>
2025-03-22 10:55:49 -0600

    Prueba no. 4

---
+++ README.md
@@ -0,0 +1,33 @@
+# Proyecto 7 app
+
+El propósito principal de este proyecto es familiarizarse con la creación y gestión de entornos virtuales de Python, así como el desarrollo y despliegue de de nuestra propia app en Render. Estas herramientas son comunes en el desarrollo de software, por lo que es esencial dominarlas.
+
+## Paso 1. Configuración
+- Creamos una cuenta en github y en render.com
+- Descargamos el paquete `streamlit` para desarrollar y desplegar nuestra app correctamente
+- Creamos un entorno virtual donde instalaremos nuestras librerías necesarias
+- Descargamos VSCode y configuramos el interprete con nuestro entorno
+
+## Paso 2. Descarga del archivo de datos
+- Descargamos `vehicles_us.csv` del enlace
+
+## Paso 3. Análisis exploratorio de datos (EDA)
+- Creamos un Jupyter notebook llamado `EDA.ipynb` 
+- Hacemos el análisis exploratorio básico de los datos
+
+## Paso 4
+- Creamos nuestra `app.py` siguiendo las instrucciones
+- Probamos nuestra app localmente con `streamlit run app.py`
+- Subimos nuestro proyecto a github
+
+## Paso 5
+- Enlazamos nuestro repositorio de github a render y configuramos el despliegue
+
+## Conclusión y URL
+Hemos completado las siguientes indicaciones:
+- Al menos un encabezado con texto.
+- Al menos un histograma.
+- Al menos un gráfico de dispersión.
+- Al menos un botón o una casilla de verificación.
+
+URL del proyecto:
--- app.py
+++ app.py
@@ -2,31 +2,65 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-# Leer los datos
+# Cargamos los datos limpios
 car_data = pd.read_csv('./vehicles_us_clean.csv')
 
-# Crear un encabezado
-st.header("Análisis de vehículos: Año, Precio y Condición")
+# Creamos el encabezado
+st.header("Análisis de vehículos")
 
-# Crear botones para generar gráficos
+# Mostramos los datos del dataframe
+st.write(car_data)
+
+# Creamos los botones y los checkbox
 hist_button = st.button('Construir histograma de precios por año de modelo')
 scatter_button = st.button('Construir gráfico de dispersión entre año de modelo y precio')
-boxplot_button = st.button('Construir gráfico de cajas por condición de vehículo')
+boxplot_checkbox = st.checkbox('Construir gráfico de cajas de precio por condición de vehículo')
+fuel_checkbox = st.checkbox('Mostrar gráfico de precio promedio por tipo de combustible')
+
 
 # Si el botón de histograma es presionado
 if hist_button:
-    # Crear un histograma de los precios por año de modelo
-    fig = px.histogram(car_data, x="model_year", y="price", histfunc="avg", title="Promedio de Precios por Año de Modelo")
+    st.write('Histograma')
+    price_avg_by_year = car_data.groupby('model_year')['price'].mean().reset_index()
+    fig = px.bar(
+        price_avg_by_year, 
+        x="model_year", 
+        y="price", 
+        title="Promedio de Precios por Año de Modelo",
+        labels={"model_year": "Año del modelo", "price": "Precio en USD"}
+    )
     st.plotly_chart(fig, use_container_width=True)
 
 # Si el botón de gráfico de dispersión es presionado
 if scatter_button:
-    # Crear un gráfico de dispersión entre año de modelo y precio, coloreado por condición
-    fig2 = px.scatter(car_data, x='model_year', y='price', color='condition', title="Precio vs Año de Modelo por Condición")
+    st.write('Gráfico de dispersión')
+    fig2 = px.scatter(
+        car_data, 
+        x='model_year', 
+        y='price', 
+        color='condition', 
+        title="Precio vs Año de modelo por condición",
+        labels={"model_year": "Año del modelo", "price": "Precio en USD", "condition": "Condición del vehículo"}
+    )
     st.plotly_chart(fig2, use_container_width=True)
 
-# Si el botón de gráfico de cajas es presionado
-if boxplot_button:
-    # Crear un gráfico de cajas que muestra la distribución de precios por condición
-    fig3 = px.box(car_data, x="condition", y="price", title="Distribución de Precios por Condición de Vehículo")
+# Si el checkbox de gráfico de cajas es presionado
+if boxplot_checkbox:
+    st.write('Boxplot')
+    fig3 = px.box(
+        car_data, 
+        x="condition", 
+        y="price", 
+        title="Precios por condición de vehículo",
+        labels={"condition": "Condición del Vehículo", "price": "Precio en USD"}
+    )
     st.plotly_chart(fig3, use_container_width=True)
+
+# Si el checkbox de gráfico de precios promedio por tipo de combustible es presionado
+if fuel_checkbox:
+    st.write('Gráfico de barras')
+    fuel_avg_price = car_data.groupby('fuel')['price'].mean().reset_index()
+    fig4 = px.bar(fuel_avg_price, x='fuel', y='price', title="Precio promedio por tipo de combustible", 
+                 labels={"fuel": "Tipo de combustible", "price": "Precio promedio en USD"}
+                 )
+    st.plotly_chart(fig4, use_container_width=True)
